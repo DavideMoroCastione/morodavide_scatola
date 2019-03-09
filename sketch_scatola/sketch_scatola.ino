48bb982a3e2fb1e54995d468034d30504a45eb9e
Davide <davide.moro@barsanti.edu.it>
2019-03-09 08:47:22 +0100

    09/03/2019 - Moro Davide

--- sketch_scatola/sketch_scatola.ino
+++ sketch_scatola/sketch_scatola.ino
@@ -1,5 +1,22 @@
+
+#define ButtonAcceso
+#define ButtonLed
+#define ButtonBuzzer
+#define Led
+#define Buzzer
+int valore1;
+int valore2;
+int valore3;
+
 void setup() {
   // put your setup code here, to run once:
+
+  valore = 0;
+  pinMode(ButtonAcceso, INPUT);
+  pinMode(ButtonLed, INPUT);
+  pinMode(ButtonBuzzer, INPUT);
+  pinMode(Led, OUTPUT);
+  pinMode(Buzzer, OUTPUT);
 
 }
 
@@ -7,3 +24,21 @@ void loop() {
   // put your main code here, to run repeatedly:
 
 }
+
+void AccendiLed() {
+  
+  valore1 = digitalRead(ButtonAcceso);
+  delay(random(1000, 10000));
+  digitalWrite(Led, HIGH);
+}
+void AccendiBuzzer() {
+  
+  valore2 = digitalRead(ButtonLed);
+  digitalWrite(Led, LOW);
+  delay(random(1000, 10000);
+  digitalWrite(Buzzer, HIGH);
+  valore3 = digitalRead(ButtonBuzzer);
+  digitalWrite(Buzzer, LOW);
+  
+}
+
