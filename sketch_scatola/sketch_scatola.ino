db6992b500f67df852b56f51244e0832a75ada78
Davide <davide.moro@barsanti.edu.it>
2019-03-13 13:53:21 +0100

    13/03/2019 - Davide Moro

--- sketch_scatola/sketch_scatola.ino
+++ sketch_scatola/sketch_scatola.ino
@@ -1,9 +1,9 @@
 
-#define ButtonAcceso
-#define ButtonLed
-#define ButtonBuzzer
-#define Led
-#define Buzzer
+#define ButtonAcceso 5
+#define ButtonLed 6
+#define ButtonBuzzer 7
+#define Led 10
+#define Buzzer 11
 int valore1;
 int valore2;
 int valore3;
@@ -11,12 +11,18 @@ int valore3;
 void setup() {
   // put your setup code here, to run once:
 
-  valore = 0;
+  valore1 = 0;
+  valore2 = 0;
+  valore3 = 0;
   pinMode(ButtonAcceso, INPUT);
   pinMode(ButtonLed, INPUT);
   pinMode(ButtonBuzzer, INPUT);
   pinMode(Led, OUTPUT);
   pinMode(Buzzer, OUTPUT);
+
+  Serial.begin(9600);
+  AccendiLed();
+  AccendiBuzzer();
 
 }
 
@@ -35,7 +41,7 @@ void AccendiBuzzer() {
   
   valore2 = digitalRead(ButtonLed);
   digitalWrite(Led, LOW);
-  delay(random(1000, 10000);
+  delay(random(1000, 10000));
   digitalWrite(Buzzer, HIGH);
   valore3 = digitalRead(ButtonBuzzer);
   digitalWrite(Buzzer, LOW);
