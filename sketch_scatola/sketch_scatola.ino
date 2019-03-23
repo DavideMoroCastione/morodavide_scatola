038cd9133c985b73a155ddcae05b14ba64fc5ad8
Davide <davide.moro@barsanti.edu.it>
2019-03-23 09:38:22 +0100

    23/03/2019 - Moro Davide

--- sketch_scatola/sketch_scatola.ino
+++ sketch_scatola/sketch_scatola.ino
@@ -1,52 +1,62 @@
 #include <LiquidCrystal.h>
 
-#define ButtonAcceso 7  // bottone inizio programma
-#define ButtonLed 8  // bottone spegnimento del led
-#define ButtonBuzzer 9  // bottone spegnimento del buzzer
-#define Led 10
-#define Buzzer 13
-int valoreTempo;  // variabile del delay del tempo
+#define buttonAcceso 7  // bottone inizio programma
+#define buttonLed 8  // bottone spegnimento del led
+#define buttonBuzzer 9  // bottone spegnimento del buzzer
+#define led 10
+#define buzzer 13
 LiquidCrystal lcd(12, 11, 6, 5, 4, 3); //lcd
-int tempo;
-
+bool fine;
+bool terminato;
 
 void setup() {
   // put your setup code here, to run once:
 
-  valoreTempo = random(1000, 10000);
-  
-  pinMode(ButtonAcceso, INPUT);
-  pinMode(ButtonLed, INPUT);
-  pinMode(ButtonBuzzer, INPUT);
-  pinMode(Led, OUTPUT);
-  pinMode(Buzzer, OUTPUT);
+  pinMode(buttonAcceso, INPUT);
+  pinMode(buttonLed, INPUT);
+  pinMode(buttonBuzzer, INPUT);
+  pinMode(led, OUTPUT);
+  pinMode(buzzer, OUTPUT);
   lcd.begin(16, 2);
+  fine = false;
+  terminato = false;
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
- 
-  while(digitalRead(ButtonAcceso) == HIGH){}
-  lcd.clear();
-  calcolaRiflessi(ButtonLed, Led); 
-  calcolaRiflessi(ButtonBuzzer, Buzzer);
-  lcd.setCursor(0, 0);
-  lcd.print(tempo);
-  lcd.setCursor(0, 1);
-  lcd.print(tempo);
+
+    while(!fine) 
+    {
+      if(digitalRead(buttonAcceso) == LOW){}
+      else if(digitalRead(buttonAcceso) == HIGH)
+      {
+        fine = true;
+      }
+    }
+    lcd.clear();
+    lcd.setCursor(0, 1);
+    accensione(buttonLed, led); 
+    lcd.setCursor(0, 2);
+    accensione(buttonBuzzer, buzzer);
   
 }
 
-void calcolaRiflessi(int btn, int thing) {
+void accensione(int btn, int thing) {
 
-  while(digitalRead(btn) == LOW){}
-  delay(valoreTempo + millis());
+  delay(random(1000, 10000));
   digitalWrite(thing, HIGH);
-  while(digitalRead(btn) == HIGH){}
-  tempo = digitalRead(btn);
+  while(!terminato) 
+  {
+    if(digitalRead(btn) == LOW) {} 
+    else if(digitalRead(btn) == HIGH)
+    {
+      terminato = true;
+    }
+  }
+  lcd.print(millis() / 1000);
   digitalWrite(thing, LOW);
-  
+ 
 }
 
 
